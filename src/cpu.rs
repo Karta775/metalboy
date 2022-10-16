de739d9fbb6e0b06915af22be45489e4b6161eee
Karta775 <Karta775@users.noreply.github.com>
2022-10-17 00:29:17 +0100

    Add Mmu to the Cpu struct
    I'm probably going to change this in the future, as of now I don't know whether the Mmu should belong to the Cpu, or if it should belong to some System struct.

--- src/cpu.rs
+++ src/cpu.rs
@@ -3,18 +3,31 @@ use super::mmu::Mmu;
 
 pub struct Cpu {
     pub reg: Registers,
+    pub mmu: Mmu,
 }
 
 impl Cpu {
     pub fn new() -> Self {
         Cpu {
             reg: Registers::new(),
+            mmu: Mmu::new(),
         }
     }
 
+    pub fn reset(&mut self) {
+        self.reg.reset();
+        self.mmu.reset();
+    }
+
     pub fn tick(&mut self) {
-        println!("CPU: tick");
         self.reg.pc += 2;
+    }
+
+    pub fn execute(&mut self) {
+        let mut bytes: usize = 1;
+        let mut advance_pc: bool = true;
+
+        // self.mmu.get(self.reg.pc);
     }
 }
 
