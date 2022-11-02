5085aa7b17164f9cf12e9f8867567f072f7097c7
Karta775 <Karta775@users.noreply.github.com>
2022-11-02 15:23:35 +0000

    Fix Cartridge::new() not always initialising 32KiB

--- src/cartridge.rs
+++ src/cartridge.rs
@@ -8,14 +8,14 @@ pub struct Cartridge {
 impl Cartridge {
     pub fn new() -> Self {
         Cartridge {
-            data: Vec::new(),
+            data: vec![0;0x8000],
         }
     }
 
     pub fn load(&mut self, rom_path: &str) {
         // TODO: Add debug traces
         let mut rom_file = File::open(&rom_path).expect("Unable to open the ROM file");
-        rom_file.read_to_end(&mut self.data).expect("Unable to read the ROM file data");
+        rom_file.read(&mut self.data).expect("Unable to read the ROM file data");
     }
 }
 
