8bc79c219e0e889cc8569ee57b7e73058da36e6c
Karta775 <Karta775@users.noreply.github.com>
2022-11-03 14:53:58 +0000

    Add execute match & func capability to generate_opcode_funcs.py
    Extra:
    - Reworked create_decode_match to generate operands if they exist

--- scripts/generate_opcode_funcs.py
+++ scripts/generate_opcode_funcs.py
@@ -1,14 +1,38 @@
 import os.path
 import json
 import urllib.request
+from argparse import ArgumentParser
 
 OPCODES_URL = "https://raw.githubusercontent.com/lmmendes/game-boy-opcodes/master/opcodes.json"
 OPCODES_FILENAME = "opcodes.json"
 
 
 def create_decode_match(values):
+    instruction = values['mnemonic'] + " "
+    if "operand1" in values.keys():
+        instruction += values['operand1'] + " "
+    if "operand2" in values.keys():
+        instruction += values['operand2']
     return '{} => Some(to_string({}, "{}", {}, "{}", "{}")),' \
-        .format(values['addr'], values['addr'], values['mnemonic'], values['length'], values['cycles'], values['group'])
+        .format(values['addr'], values['addr'], instruction, values['length'], values['cycles'], values['group'])
+
+
+def create_execute_match(values):
+    return '{} => execute_{}(cpu),'.format(values['addr'], values['addr'][2:])
+
+
+def create_execute_func(values):
+    if len(values['cycles']) > 1:
+        cycles = "// Two possible CPU cycles: {}".format(values['cycles'])
+    else:
+        cycles = "cpu.cycles += {}".format(values['cycles'][0])
+    return \
+        '''fn execute_{}(cpu: &mut Cpu) {{
+    op_unimplemented(cpu);
+    cpu.advance_pc = {};
+    {};
+}} // {} [{}/{}/{}/{}]'''.format(values['addr'][2:], values['length'], cycles, values['mnemonic'],
+                                 values['flags'][0], values['flags'][1], values['flags'][2], values['flags'][3])
 
 
 def main():
@@ -21,9 +45,27 @@ def main():
         unprefixed = opcodes['unprefixed']
         cbprefixed = opcodes['cbprefixed']
 
-        # Print out every decode match (unprefixed)
+        # Divide all cycle numbers by 4
         for opcode, values in unprefixed.items():
-            print(create_decode_match(values))
+            for i in range(len(values['cycles'])):
+                values['cycles'][i] = int(values['cycles'][i] / 4)
+
+        if args.decode_matches:  # Print out every decode match (unprefixed)
+            for opcode, values in unprefixed.items():
+                print(create_decode_match(values))
+        elif args.execute_matches:  # Print out every execute match (unprefixed)
+            for opcode, values in unprefixed.items():
+                print(create_execute_match(values))
+        elif args.execute_funcs:  # Print out every execute function stub (unprefixed)
+            for opcode, values in unprefixed.items():
+                print(create_execute_func(values))
+        else:
+            parser.print_help()
 
 
+parser = ArgumentParser()
+parser.add_argument("--decode-matches", action="store_true", help="Print out the decode matches")
+parser.add_argument("--execute-matches", action="store_true", help="Print out the execute matches")
+parser.add_argument("--execute-funcs", action="store_true", help="Print out the execute function stubs")
+args = parser.parse_args()
 main()
