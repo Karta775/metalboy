0ebf4476e1725ff0ee8c0a2a0c8e38e75017bf3f
Karta775 <Karta775@users.noreply.github.com>
2022-11-03 10:41:11 +0000

    Create disassembler (decode.rs)
    decode.rs contains mainly a large match expression generated via generate_opcode_funcs.py.

--- scripts/generate_opcode_funcs.py
+++ scripts/generate_opcode_funcs.py
@@ -1,9 +1,29 @@
 import os.path
+import json
 import urllib.request
 
 OPCODES_URL = "https://raw.githubusercontent.com/lmmendes/game-boy-opcodes/master/opcodes.json"
 OPCODES_FILENAME = "opcodes.json"
 
-if not os.path.exists(OPCODES_FILENAME):
-    print("No opcodes.json is present, downloading the latest version...")
-    urllib.request.urlretrieve(OPCODES_URL, OPCODES_FILENAME)
+
+def create_decode_match(values):
+    return '{} => print_decode(cpu.opcode, "{}", {}, "{}", "{}"),' \
+        .format(values['addr'], values['mnemonic'], values['length'], values['cycles'], values['group'])
+
+
+def main():
+    if not os.path.exists(OPCODES_FILENAME):
+        print("No opcodes.json is present, downloading the latest version...")
+        urllib.request.urlretrieve(OPCODES_URL, OPCODES_FILENAME)
+
+    with open(OPCODES_FILENAME) as opcodes_json:
+        opcodes = json.load(opcodes_json)
+        unprefixed = opcodes['unprefixed']
+        cbprefixed = opcodes['cbprefixed']
+
+        # Print out every decode match (unprefixed)
+        for opcode, values in unprefixed.items():
+            print(create_decode_match(values))
+
+
+main()
--- src/cpu.rs
+++ src/cpu.rs
@@ -1,9 +1,11 @@
 use super::registers::Registers;
 use super::mmu::Mmu;
+use super::decode::decode;
 
 pub struct Cpu {
     pub reg: Registers,
     pub mmu: Mmu,
+    pub opcode: u8,
 }
 
 impl Cpu {
@@ -11,6 +13,7 @@ impl Cpu {
         Cpu {
             reg: Registers::new(),
             mmu: Mmu::new(),
+            opcode: 0x00,
         }
     }
 
@@ -20,12 +23,18 @@ impl Cpu {
     }
 
     pub fn tick(&mut self) {
-        if self.reg.pc % 80 == 0 {
-            println!();
-        }
-        print!("{:02x} ", self.mmu.get(self.reg.pc));
+        // Debugging
+        // if self.reg.pc % 80 == 0 {
+        //     println!();
+        // }
+        // print!("{:02x} ", self.mmu.get(self.reg.pc));
 
+        self.opcode = self.mmu.get(self.reg.pc);
+        decode(self);
         self.reg.pc += 2;
+        if self.reg.pc > 0x104 {
+            panic!();
+        }
     }
 
     pub fn execute(&mut self) {
